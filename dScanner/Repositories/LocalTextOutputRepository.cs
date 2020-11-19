113619cc798b6ff8ceb716c0194c2a8fc443d6cb
gus.gray <augustine.h.gray@gmail.com>
2020-11-18 16:19:53 -0800

    a

---
+++ dScanner/Repositories/LocalTextOutputRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dScanner.Repositories
+{
+    class LocalTextOutputRepository
+    {
+        public string outputFileName;
+        public LocalTextOutputRepository(string outputFileName)
+        {
+            this.outputFileName = outputFileName;
+        }
+
+        public void RecordLog(string[] log)
+        {
+            System.IO.File.AppendAllLines(outputFileName, log); 
+        }
+    }
+}
